74c20b2c672d7265590f4c82fd5e35fd45730800
RyhorPaprauko <ryhorpaprauka@gmail.com>
2019-10-28 10:05:11 +0300

    corrections

--- src/main/java/by/itacademy/documentmanager/config/DropBoxConfig.java
+++ src/main/java/by/itacademy/documentmanager/config/DropBoxConfig.java
@@ -2,7 +2,6 @@ package by.itacademy.documentmanager.config;
 
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -10,7 +9,6 @@ public class DropBoxConfig {
 
     private static final String ACCESS_TOKEN = "WopTJHwC14AAAAAAAAABYtxDMuNcdXzQj-h7M5m2nyQxLj8nT6N3nOQRcOlFGrH0";
 
-    @Bean("dropBoxClient")
     public DbxClientV2 dropBoxClient() {
         DbxRequestConfig config = new DbxRequestConfig("dropbox/java-tutorial");
         return new DbxClientV2(config, ACCESS_TOKEN);
